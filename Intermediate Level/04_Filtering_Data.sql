e7c714dda8a38774666bd2538fc76b1b18588195
Sambhav <sambhav12wakhariya@gmail.com>
2025-11-18 13:45:45 +0530

    Checked In : Comparison Operators

--- Intermediate Level/04_Filtering_Data.sql
+++ Intermediate Level/04_Filtering_Data.sql
@@ -17,3 +17,36 @@
         - LIKE
 =================================================================================
 */
+/* ============================================================================== 
+   COMPARISON OPERATORS
+=============================================================================== */
+
+-- Retrieve all customers from Germany.
+SELECT *
+FROM customers
+WHERE country = 'Germany'
+
+-- Retrieve all customers who are not from Germany.
+SELECT *
+FROM customers
+WHERE country <> 'Germany'
+
+-- Retrieve all customers with a score greater than 500.
+SELECT *
+FROM customers
+WHERE score > 500
+
+-- Retrieve all customers with a score of 500 or more.
+SELECT *
+FROM customers
+WHERE score >= 500
+
+-- Retrieve all customers with a score less than 500.
+SELECT *
+FROM customers
+WHERE score < 500
+
+-- Retrieve all customers with a score of 500 or less.
+SELECT *
+FROM customers
+WHERE score <= 500
